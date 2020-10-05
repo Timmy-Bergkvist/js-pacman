009d5f4dcdbcfc123718b099ebb7588a7bbf09d7
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-10-05 17:52:00 +0200

    setting up files and functions

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS Pacman
+
+JS Pacman a javascript game.
+
+## Credits
+
+the code and tutorial for this game can you find here:
+
+https://github.com/weibenfalk/vanilla-js-pacman
+https://www.youtube.com/watch?v=YBtzzVwrTeE&list=PLZMWJltr6TmCekz4-mY0_fec19HVS8G6W&index=38&t=1s
+
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup';
+
+// DOM Element
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
+
+// Game Constants
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80;
+
+// Initial setup
+let score = 0;
+let timer = null;
+let gameWin = false;
+let PowerPillActive = false;
+let PowerPillTimer = null;
+
+function gameOver(pacman, grid) {
+
+
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+    
+}
