f413e4a3e2a903d99ab2e84cd5bd5992ae0e7785
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-10-19 11:26:05 +0200

    rearrange the js files

--- dist/js-pacman.e31bb0bc.js
+++ dist/js-pacman.e31bb0bc.js
@@ -820,7 +820,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55451" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64446" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
